4538135c1d100fe0ea90cbec49f183d0932e53e5
Facu00H <faisher00@gmail.com>
2022-12-22 16:39:43 -0300

    documentacion metodos post & delete

--- routes/entradaSalida.js
+++ routes/entradaSalida.js
@@ -9,7 +9,7 @@ const router = express.Router();
  * @openapi
  * /marcarHorario/{userId}:
  *   post:
- *     summary: peticion para crear una empresa, se requiere pasar por query el id del owner
+ *     summary: peticion para crear un ingreso o egreso
  *     tags:
  *       - EntradaSalida
  *     parameters:
@@ -56,6 +56,54 @@ const router = express.Router();
  *                     example: false
  */
 router.post('/:userId', marcarIngresoEgreso);
-router.delete('/:userId/:entradaId', eliminarEntradaSalida);
+/**
+ * @openapi
+ * /marcarHorario/{userId}/{entradaSalidaId}:
+ *   delete:
+ *     summary: peticion para eliminar un ingreso o egreso
+ *     tags:
+ *       - EntradaSalida
+ *     parameters:
+ *       - name: userId
+ *         required: true
+ *         in: path
+ *         schema:
+ *           type: string
+ *         description: id del empleado a eliminarle la entrada/salida
+ *       - name: entradaSalidaId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: id de la entrada/salida a eliminar
+ *     responses:
+ *       200:
+ *         description: Entrada o Salida eliminada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 response:
+ *                   type: string
+ *                   example: entrada/salida eliminada de manera exitosa
+ *                 succes:
+ *                   type: boolean
+ *                   example: true
+ *       400:
+ *         description: Invalid request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  response:
+ *                     type: string
+ *                     example: X mesaje de error
+ *                  succes:
+ *                     type: boolean
+ *                     example: false
+ */
+router.delete('/:userId/:entradaSalidaId', eliminarEntradaSalida);
 
 module.exports = router;
