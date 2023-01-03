e24cf5c8b7f9d581b2d7c1b605519b23fa9e3de4
Nicolás Bentancor <bentanico@gmail.com>
2023-01-03 16:41:25 -0300

    creacion de pdf recibo de sueldos

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 const config = require('config');
 const cors = require('cors');
 const createError = require('http-errors');
@@ -45,5 +47,52 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// creacion de recibos de sueldo en formato .pdf
+// Create a document
+const doc = new PDFDocument();
+
+// Pipe its output somewhere, like to a file or HTTP response
+// See below for browser usage
+doc.pipe(fs.createWriteStream('./output/sueldos.pdf'));
+
+// Embed a font, set the font size, and render some text
+doc
+  //.font('./public/stylesheets/style.css/body.font') // TODO: insertar fuente
+  .fontSize(25)
+  .text('Recibo de sueldo', 100, 100);
+
+// Add an image, constrain it to a given size, and center it vertically and horizontally
+doc.image('./public/images/sueldo.png', {
+  fit: [250, 300],
+  align: 'center',
+  valign: 'center',
+});
+
+// Add another page
+doc.addPage().fontSize(25).text('Liquidación de sueldo...', 100, 100);
+
+// comentado xq quizas luego sirve
+// // Draw a triangle
+// doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill('#FF3300');
+
+// // Apply some transforms and render an SVG path with the 'even-odd' fill rule
+// doc
+//   .scale(0.6)
+//   .translate(470, -380)
+//   .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+//   .fill('red', 'even-odd')
+//   .restore();
+
+// Add some text with annotations
+doc
+  .addPage()
+  .fillColor('blue')
+  .text('Link a la app de SoftGestion!', 100, 100)
+  .underline(100, 100, 160, 27, { color: '#0000FF' })
+  .link(100, 100, 160, 27, 'http://google.com/'); // TODO: insertar nuestro link
+
+// Finalize PDF file
+doc.end();
 
 module.exports = app;
